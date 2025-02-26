330d5f0c90eb1474898749956bd22e3619255111
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-02-26 18:38:26 +0530

    doctor page

--- src/pages/Doctors/DoctorDetails.jsx
+++ src/pages/Doctors/DoctorDetails.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
+import DoctorAbout from "./DoctorAbout";
+import Feedback from "./Feedback";
 
 const DoctorDetails = () => {
+  const [tab, setTab] = useState("about");
+
   return (
     <>
       <section>
@@ -17,7 +22,7 @@ const DoctorDetails = () => {
                     Surgeon
                   </span>
                   <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
-                    Meet Sethiya
+                    Shreyansh Rane
                   </h3>
                   <div className="flex items-center gap-[6px]">
                     <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
@@ -27,7 +32,37 @@ const DoctorDetails = () => {
                       (272)
                     </span>
                   </div>
+                  <p className="text__para text-[14px] leading-6 md:text-[15px] max-w-[390px]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Deleniti, minus.
+                  </p>
                 </div>
+              </div>
+              <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+                <button
+                  onClick={() => setTab("about")}
+                  className={`py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold ${
+                    tab === "about"
+                      ? "border-b border-solid border-primaryColor"
+                      : ""
+                  }`}
+                >
+                  About
+                </button>
+                <button
+                  onClick={() => setTab("feedback")}
+                  className={`py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold ${
+                    tab === "feedback"
+                      ? "border-b border-solid border-primaryColor"
+                      : ""
+                  }`}
+                >
+                  Feedback
+                </button>
+              </div>
+              <div className="mt-[50px]">
+                {tab === "about" ? <DoctorAbout /> : ""}
+                {tab === "feedback" ? <Feedback /> : ""}
               </div>
             </div>
             <div className=""></div>
